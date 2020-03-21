e6db25fdde4462c3d82476af0218329fceb95068
Ankit Ramchandani <ankit61@tamu.edu>
2020-03-21 18:55:18 -0500

    adding cough dataset

---
+++ CoughDataset.py
@@ -0,0 +1,95 @@
+from torch.utils.data import Dataset
+import constants
+import torchvision.io as io
+import os
+import torchaudio.transforms as AT
+import torchvision.transforms as IT
+import json
+import torch
+
+class VideoTransform:
+    def __init__(self, f):
+        self.f = f
+
+    def __call__(self, v):
+        ans = []
+        for img in v:
+            out = self.f(img)
+            if torch.is_tensor(out):
+                out = torch.unsqueeze(out, dim=0)
+            
+            ans += [out]
+
+        return torch.cat(ans, dim=0) if torch.is_tensor(ans[0]) else ans
+
+class ReduceAudioChannels():
+    def __init__(self):
+        pass
+    
+    def __call__(self, a):
+        return torch.mean(a, dim=0, keepdim=True)
+
+class NormalizeAudio:
+    def __init__(self):
+        pass
+    
+    def __call__(self, a):
+        return a.div(1<<31)
+
+class CoughDataset(Dataset):
+    def __init__(self, root_dir = constants.DATA_BASE_DIR, chunk_size = constants.CHUNK_SIZE):
+        fs = [f for f in os.listdir(root_dir) if f.endswith('_rs.mp4')]
+        self.data = []
+        labels = json.loads(open(os.path.join(root_dir, 'labels.json'), 'r').read())
+
+        self.audio_transforms = IT.Compose([
+            ReduceAudioChannels(),
+            NormalizeAudio(),
+            AT.Resample(constants.AUDIO_SAMPLE_RATE, constants.RESAMPLED_AUDIO_SAMPLE_RATE),
+            AT.MelSpectrogram(sample_rate=constants.RESAMPLED_AUDIO_SAMPLE_RATE)
+        ])
+
+        self.video_transforms = IT.Compose([
+            VideoTransform(IT.ToPILImage()),
+            VideoTransform(IT.Resize(constants.INPUT_FRAME_WIDTH)),
+            #add horizontal flipping if needed
+            VideoTransform(IT.ToTensor())
+        ])
+
+        for f in fs:
+            #break in 1 sec chunks and add label
+            chunks = self.break_in_chunks(os.path.join(root_dir, f), labels[f], chunk_size)
+            self.data += chunks
+
+    def __len__(self):
+        return len(self.data)
+
+    def __getitem__(self, idx):
+        if torch.is_tensor(idx):
+            idx = idx.tolist()
+
+        return self.data[idx]
+
+    def break_in_chunks(self, f, cough_times, chunk_size):
+        v, a, meta = io.read_video(f, pts_unit='sec')
+        v = v.permute([0, 3, 1, 2])
+        print(v.shape)
+
+        ans = []
+        #break into chunks
+        end_frame = int(v.shape[0] / constants.VIDEO_FPS) * constants.VIDEO_FPS
+        vid_range = range(0, end_frame, constants.VIDEO_FPS)
+        
+        end_audio_frame = int(a.shape[1] / constants.AUDIO_SAMPLE_RATE) * constants.AUDIO_SAMPLE_RATE
+        audio_range = range(0, end_audio_frame, constants.AUDIO_SAMPLE_RATE)
+
+        for i, (v_frame, a_frame) in enumerate(zip(vid_range, audio_range)): 
+            v_chunk = v[v_frame:v_frame + constants.VIDEO_FPS]
+            a_chunk = a[:, a_frame:a_frame + constants.AUDIO_SAMPLE_RATE]
+            
+            #apply transforms
+            ans.append(
+                (self.video_transforms(v_chunk), self.audio_transforms(a_chunk).squeeze(), 1 if i in cough_times else 0)
+            )
+
+        return ans
--- constants.py
+++ constants.py
@@ -4,12 +4,23 @@ import getpass
 from datetime import datetime
 import multiprocessing
 
-EPOCHS                      = 300
+NUM_CLASSES                 = 2
 
 PRINT_FREQ                  = 20
 INTERMITTENT_OUTPUT_FREQ    = 5 # Num batches between outputs
 
-BATCH_SIZE              = 64
+#HYPERPARAMETERS
+BATCH_SIZE                  = 64
+EPOCHS                      = 300
+LR                          = 0.05
+WEIGHT_DECAY                = 0
+
+#DATASET
+CHUNK_SIZE                  = 1
+AUDIO_SAMPLE_RATE           = 48000
+RESAMPLED_AUDIO_SAMPLE_RATE = 16000
+INPUT_FRAME_WIDTH           = 100
+VIDEO_FPS                   = 30
 
 MIN_LEARNING_RATE           = 0.000001
 
@@ -17,5 +28,4 @@ TENSORBOARDX_BASE_DIR = os.path.join(
     Path(os.path.dirname(os.path.abspath(__file__))).parent, os.path.join('runs', datetime.now().strftime('%b%d_%H-%M-%S') + '_' + getpass.getuser()))
 MODELS_BASE_DIR = os.path.join(
     Path(os.path.dirname(os.path.abspath(__file__))).parent, 'models')
-DATA_BASE_DIR = os.path.join(
-    Path(os.path.dirname(os.path.abspath(__file__))).parent, 'data')
+DATA_BASE_DIR = '/media/sf_data'
