ec9003a9e13b52ed444218a10680010cc852e024
Ankit Ramchandani <ankit61@tamu.edu>
2020-03-22 20:08:09 -0500

    hyperparams

--- constants.py
+++ constants.py
@@ -13,7 +13,7 @@ SAVE_FREQ                   = 5
 #HYPERPARAMETERS
 BATCH_SIZE                  = 32
 EPOCHS                      = 300
-LR                          = 0.05
+LR                          = 0.01
 WEIGHT_DECAY                = 4e-4
 
 #DATASET
