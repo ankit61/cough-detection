a3579e5cff2586d7ad27dfa750146e9cb4e8a827
Ankit Ramchandani <ankit61@tamu.edu>
2020-04-11 16:28:46 -0500

    doing conv3d

--- ResNet3DRunner.py
+++ ResNet3DRunner.py
@@ -4,19 +4,11 @@ import constants
 import torch.nn as nn
 
 def get_visual_model():
-    from torchvision.models import resnet18
-
-    net = resnet18(pretrained=True)
-    net.conv1 = nn.Conv2d(constants.VIDEO_FPS * constants.CHUNK_SIZE * 3, 64, kernel_size=7, stride=2, padding=3, bias=False)
-    net.bn1 = nn.BatchNorm2d(64)
-
-    return net
-
-    #return resnet10(
-    #        num_classes=constants.NUM_CLASSES, 
-    #        sample_duration=constants.VIDEO_FPS, 
-    #        sample_size=constants.INPUT_FRAME_WIDTH
-    #    )
+    return resnet10(
+            num_classes=constants.NUM_CLASSES, 
+            sample_duration=constants.VIDEO_FPS, 
+            sample_size=constants.INPUT_FRAME_WIDTH
+        )
 
 class ResNet3DRunner(BaseResNetRunner):
     def __init__(self, load_paths=None):
